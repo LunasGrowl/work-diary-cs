98f717933345379ae403484d2847b02ce2840386
Administrator <Administrator@EC2AMAZ-GH1MO3S>
2024-09-09 09:00:09 +0000

    First commit

---
+++ Backend/Context/LogContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
+using Backend.Models;
+
+namespace Backend.Context
+{
+    public class LogContext : DbContext
+    {
+
+        public LogContext(DbContextOptions<LogContext> options):base(options){}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Entry>();
+            modelBuilder.Seed();
+        }
+
+        public DbSet<Entry> Entries { get; set; }
+    }
+}
---
+++ Backend/Controllers/EntryController.cs
@@ -0,0 +1,81 @@
+﻿using Backend.Context;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Backend.Models;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EntryController : ControllerBase
+    {
+
+        private readonly LogContext _logContext;
+
+        public EntryController(LogContext logContext)
+        {
+            _logContext = logContext;
+
+            _logContext.Database.EnsureCreated();
+        }
+
+        [HttpGet]
+        public ActionResult getAllEntry()
+        {
+            return Ok(_logContext.Entries.ToArray());
+        }
+
+        [HttpGet, Route("{id}")]
+        public ActionResult getEntry(int id)
+        {
+            var entry = _logContext.Entries.Find(id);
+            if (entry == null)
+            {
+                return NotFound();
+            }
+            return Ok(entry);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> postEntry(Entry entry)
+        {
+            _logContext.Entries.Add(entry);
+            await _logContext.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpPut, Route("{id}")]
+        public async Task<ActionResult> putEntry(int id, Entry entry)
+        {
+            _logContext.Entry(entry).State = EntityState.Modified;
+
+            if (id != entry.Id) 
+            {
+                return NotFound();
+            }
+
+            await _logContext.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete, Route("{id}")]
+        public async Task<ActionResult> deleteEntry(int id)
+        {
+            var entry = await _logContext.Entries.FindAsync(id);
+
+            if(entry == null)
+            {
+                return NotFound();
+            }
+
+            _logContext.Entries.Remove(entry);
+            await _logContext.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
